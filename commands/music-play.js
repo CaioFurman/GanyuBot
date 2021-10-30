2f3b685486760ebf6d69dda51ef50856bfeebf02
kns <kannesf@gmail.com>
2021-10-30 01:48:32 -0300

    queue created

--- commands/music-play.js
+++ commands/music-play.js
@@ -8,44 +8,93 @@ const {
 	joinVoiceChannel,
 } = require('@discordjs/voice');
 
+const queue = new Map();
 
 module.exports = {
     name: 'play',
     description: "Plays youtube videos",
     async execute(message, args) {
         const voiceChannel = message.member.voice.channel;
+        const connection = joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: voiceChannel.guild.id,
+            adapterCreator: voiceChannel.guild.voiceAdapterCreator
+        });
 
         if (!voiceChannel) return message.reply('Please, join a voice channel.')
         if (!args.length) return message.reply('Please, enter what you want to listen or send a URL.')
 
-        const connection = joinVoiceChannel({
-                channelId: voiceChannel.id,
-                guildId: voiceChannel.guild.id,
-                adapterCreator: voiceChannel.guild.voiceAdapterCreator
-            });
+        const server_queue = queue.get(message.guild.id);
+        let song = {};
+        if (ytDownload.validateURL(args[0])) {
+            const song_info = await ytDownload.getInfo(args[0]);
+            song = { title: song_info.videoDetails.title, url: song_info.videoDetails.video_url }
 
-        const videoFinder = async (query) => {
-            const videoResult = await ytSearch(query);
+        } else {            
+            const videoFinder = async (query) => {
+                const videoResult = await ytSearch(query);
+                return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
+            }
 
-            return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
+            const video = await videoFinder(args.join(' '));
+            if (video){
+                song = { title: video.title, url: video.url}
+
+            } else {
+                message.reply('No results found.')
+                connection.destroy();
+            }
         }
 
-        const video = await videoFinder(args.join(' '));
+        if (!server_queue) {
 
-        if (video){
-            const stream = ytDownload(video.url, {filter: 'audioonly'});
-            const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
-            const player = createAudioPlayer();
+            const queue_constructor = {
+                voiceChannel: voiceChannel,
+                textChannel: message.channel,
+                connection: null,
+                songs: []
+            }
 
-            player.play(resource);
-            connection.subscribe(player);
-            await message.reply(`Now playing: **${video.title}**`)
+            queue.set(message.guild.id, queue_constructor);
+            queue_constructor.songs.push(song);
 
-            player.on(AudioPlayerStatus.Idle, () => connection.destroy());
-                
+            try {
+                queue_constructor.connection = connection;
+                video_player(message.guild, queue_constructor.songs[0]);
+            } catch (err) {
+                queue.delete(message.guild.id);
+                message.channel.send('There was an error...')
+                throw err;
+            }
         } else {
-            message.reply('No results found.')
-            connection.destroy();
+            server_queue.songs.push(song);
+            return message.channel.send(`***${song.title}*** added to the queue.`);
         }
     }
+}
+
+const video_player = async (guild, song) => {
+    const song_queue = queue.get(guild.id);
+
+    if (!song) {
+        song_queue.connection.destroy();
+        queue.delete(guild.id);
+        return;
+    }
+    const stream = ytDownload(song.url, {filter: 'audioonly'});
+    const resource = createAudioResource( stream, { inputType: StreamType.Arbitrary });
+    const player = createAudioPlayer(); 
+    song_queue.connection.subscribe(player)
+    player.play(resource);
+
+    player.on(AudioPlayerStatus.Idle, () => {
+        song_queue.songs.shift();
+        video_player(guild, song_queue.songs[0]);
+    });
+    await song_queue.textChannel.send(`Now playing: ***${song.title}***.`)
+
+    player.on('error', error => {
+        console.error(`Error: ${error.message} with resource ${error.resource.metadata}`);
+        player.play(getNextResource());
+    })
 };
