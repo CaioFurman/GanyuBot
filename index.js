4d9ab91e8b096216ff28cda3428f494ef2b414e2
Caio Furman <85889752+CaioFurman@users.noreply.github.com>
2021-10-30 02:45:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ const { token, prefix } = require('./config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES]});
 const fs = require('fs')
 
-client.commands = new Collection();
 
 // import commands
+client.commands = new Collection();
+
 const botCommands = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of botCommands){
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
 
+
 // initialize Ganyu
 client.once('ready', () => {
 	console.log(`Ready! \nWorking on ${client.guilds.cache.size} servers.`);
 	client.user.setStatus('online');
     client.user.setActivity(`${client.guilds.cache.size} servers, !!help for info.`, {
-      type: 'WATCHING',
+    	type: 'WATCHING',
     })
 })
 
@@ -25,7 +27,7 @@ client.once('ready', () => {
 client.on('guildCreate', guild => {
 	console.log(`Joined a new server:\n${guild.name} (${guild.id})\n${guild.cache.memberCount} members.`);
     client.user.setActivity(`${client.guilds.cache.size} servers, !!help`, {
-      type: 'WATCHING',
+    	type: 'WATCHING',
     })
 })
 
@@ -33,11 +35,12 @@ client.on('guildCreate', guild => {
 client.on('guildDelete', guild => {
 	console.log(`Removed from the server:\n${guild.name} (${guild.id})`);
     client.user.setActivity(`${client.guilds.cache.size} servers, !!help for info.`, {
-      type: 'WATCHING',
+    	type: 'WATCHING',
     })
 })
 
-// test using slashcommands
+
+// test using slashcommands (going to be deleted in the future)
 // registered in ./deploy-commands.js
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
@@ -46,8 +49,8 @@ client.on('interactionCreate', async interaction => {
 
 	if (commandName === 'ping') {
 		await interaction.reply('Pong!');
-    } else if (commandName === 'beep') {
-        await interaction.reply('Boop :robot:');
+    	} else if (commandName === 'beep') {
+        	await interaction.reply('Boop :robot:');
 	} else if (commandName === 'server') {
 		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
 	} else if (commandName === 'user') {
@@ -55,13 +58,16 @@ client.on('interactionCreate', async interaction => {
 	}
 });
 
+
 // LIST OF BOT COMMANDS!
-// everything is stored at ./commands/
+
+// requiring prefix
 client.on('messageCreate', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return; 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
+// codes stored at ./commands/
     if (command === 'help') {
         client.commands.get('help').execute(message, args);
     } else if (command === 'play') {
@@ -69,7 +75,7 @@ client.on('messageCreate', message => {
     } else if (command === 'leave') {
         client.commands.get ('leave').execute(message, args);
     } else if (command === 'clear') {
-      client.commands.get ('clear').execute(message, args);
+      	client.commands.get ('clear').execute(message, args);
     } 
  });
 
